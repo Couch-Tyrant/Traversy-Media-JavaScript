2d720359107f5678c8935e435b2ddc5df8b93dc8
Couch Tyrant <130974421+Couch-Tyrant@users.noreply.github.com>
2023-09-29 12:18:13 +0530

    Comments & Shortcuts

---
+++ 01-variables-data-types/02-comments-and-shortcuts/script.js
@@ -0,0 +1,10 @@
+// console.log(100); Single line comment
+console.error('aaaaaaaaa');
+console.warn("Warning")
+console.table({name:"arg",age:56,gender:'N/A'})
+/* Multi line comment
+const style='padding:10em;background-color:orange;color:white;'
+console.log("%c Hello",style);
+*/
+const style='padding:10em;background-color:orange;color:white;'
+console.log("%c Hello",style);

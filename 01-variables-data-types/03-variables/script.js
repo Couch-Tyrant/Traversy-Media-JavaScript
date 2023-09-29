56cad3dd640e1477304045614d0b6a844c56deb5
Couch Tyrant <130974421+Couch-Tyrant@users.noreply.github.com>
2023-09-29 13:46:41 +0530

    Variables

---
+++ 01-variables-data-types/03-variables/script.js
@@ -0,0 +1,62 @@
+// var a='a'
+// console.log(a);
+// var a='b'
+
+// LET and VAR diffrence
+// let greeting = "say Hi";
+// let times = 4;
+
+// if (times > 3) {
+//      let hello = "say Hello instead";
+//      console.log(hello);// "say Hello instead"
+//  }
+// console.log(hello) // hello is not defined
+
+// var greeting = "say Hi";
+// var times = 4;
+// // function fun(){
+// // if (times > 3) {
+// //     var hello = "say Hello instead";
+// //     console.log(hello);// "say Hello instead"
+// // }
+// // }
+// // fun();
+// if (times > 3) {
+//     var hello = "say Hello instead";
+//     console.log(hello);// "say Hello instead"
+// }
+// console.log(hello) // hello is not defined
+
+// var a='a'
+// console.log(a);
+// var a='b'
+
+// let b ='a'
+// console.log(b);
+// let b ='a'
+
+// console.log(a);
+// const a="aaaaaaaaaaa"
+// a='b'
+
+let firstName = 'john';
+let lastName = 'Doe';
+console.log(firstName, lastName, age);
+var age = 15;
+
+let $andy3 = 'dog';
+console.log($andy3);
+
+//re-assigning variables
+age = 31;
+console.log(age);
+
+const person = { name: 'john doe', age: 23 };
+console.log(person.name);
+
+let a, b, c;
+const d = 45,
+  e = 50,
+  f = 90;
+
+console.log(a);

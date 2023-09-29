69e295b3a61d1e5b083e883d010687758fed6ad0
Couch Tyrant <130974421+Couch-Tyrant@users.noreply.github.com>
2023-09-29 11:47:03 +0530

    console

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,8 @@
+console.log(100);
+console.error('aaaaaaaaa');
+console.warn("Warning")
+console.table({name:"arg",age:56,gender:'N/A'})
+
+const style='padding:10em;background-color:orange;color:white;'
+console.log("%c Hello",style);
+

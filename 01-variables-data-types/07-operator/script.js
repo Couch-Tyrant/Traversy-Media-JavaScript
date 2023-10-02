0ce72148fa66a46a8cf1d5433101ed2155df020b
Shubham Singh <shubhamsingh290596@gmail.com>
2023-10-02 20:21:44 +0530

    Operator

---
+++ 01-variables-data-types/07-operator/script.js
@@ -0,0 +1,49 @@
+// 1. Arithemetic
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 4 % 5;
+
+// 2.Concatination
+x = "Hello" + "World";
+
+// 3.Exponent
+x = 2 ** 3;
+
+// 4.Increment
+x = 1;
+x++;
+++x;
+
+// 5.Decrement
+x = 1;
+x--;
+--x;
+
+// 6.Assigment
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 4;
+x **= 3;
+
+// 7.Comparison
+x = 2 == "2";
+x = 2 === "2";
+
+x = 2 != "2";
+x = 2 !== "2";
+
+x = 10 > 5
+x = 10 < 5
+x = 10 >= 5
+x = 10 <= 5
+
+
+console.log(x);

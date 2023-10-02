65fdf421473b37aa3397898b5887545b466f1994
Shubham Singh <shubhamsingh290596@gmail.com>
2023-10-02 20:29:52 +0530

    Type Coersion

---
+++ 01-variables-data-types/08-type-coersion/script.js
@@ -0,0 +1,13 @@
+let x;
+
+x = 5 + "5";
+x = 5 * "5";
+x = 5 + null;
+x = Number(null);
+x = Number(true);
+x = Number(false);
+
+x = 5 + Number(true);
+x = 5 + undefined;
+
+console.log(x, typeof x);

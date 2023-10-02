5328d69e83fd90bc71fd3a5db4fe584c333ca437
Shubham Singh <shubhamsingh290596@gmail.com>
2023-10-02 19:55:51 +0530

    Type Conversion

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,18 @@
+let number = 0;
+
+// Change string -> number
+// number = parseInt(number)
+// number = +number
+// number = Number(number);
+
+// Change number -> string
+// number = number.toString()
+// number = String(number);
+
+// Change string -> float
+// number = parseFloat(number)
+
+// Change number -> boolean
+number = Boolean(number)
+
+console.log(number, typeof number);

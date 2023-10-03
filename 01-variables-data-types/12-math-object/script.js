0c0a722f57fe786970b17b392548794b3269b8d4
Couch Tyrant <130974421+Couch-Tyrant@users.noreply.github.com>
2023-10-03 13:07:34 +0530

    Math Object

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,21 @@
+let x;
+
+x = Math.sqrt(4);
+
+x = Math.abs(-98);
+
+x = Math.round(69.6);
+
+x = Math.ceil(69.6);
+
+x = Math.floor(69.6);
+
+x = Math.pow(2, 3);
+
+x = Math.min(...[1, 2, 3, 4], 5, -6);
+
+x = Math.random();
+
+x = Math.floor(Math.random() * 10 + 1);
+
+console.log(x, typeof x);

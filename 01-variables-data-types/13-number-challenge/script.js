66f44dafe95544cee92bbaec63a258a337f1745b
Couch Tyrant <130974421+Couch-Tyrant@users.noreply.github.com>
2023-10-03 13:23:07 +0530

    Number Challenge

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,16 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+let sumOutput = `${x} + ${y} = ${x + y}`;
+let diffrenceOutput = `${x} - ${y} = ${x - y}`;
+let productOutput = `${x} * ${y} = ${x * y}`;
+let quotientOutput = `${x} / ${y} = ${x / y}`;
+let remainderOutput = `${x} % ${y} = ${x % y}`;
+
+console.log(x);
+console.log(y);
+console.log(sumOutput);
+console.log(diffrenceOutput);
+console.log(productOutput);
+console.log(quotientOutput);
+console.log(remainderOutput);
